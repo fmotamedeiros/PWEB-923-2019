d95fe2649bb097a129e6af05118992659ddb3751
Flávio Medeiros <flaviomotamedeiros@gmail.com>
2019-05-22 15:00:17 -0300

    Exemplo inicial de frontend e backend em JS.

---
+++ Exemplo FrontBack JS/servidor/servidor.js
@@ -0,0 +1,55 @@
+const express = require('express')
+var favicon = require('serve-favicon');
+var mysql = require('mysql');
+
+const app = express()
+const port = 3000
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.get('/', (request, response) => {
+  console.log('Recebendo a requisição!')
+  response.send('Hello from Express!')
+})
+
+app.get('/hello', (request, response) => {
+  console.log('Recebendo a requisição Hello!')
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/cadastro', (request, response) => {
+    console.log('Recebendo a requisição de cadastro!')
+    
+    var email = request.query.email
+    var senha = request.query.senha
+    var nome = request.query.nome
+
+    // Banco de Dados
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'admin',
+      database : 'pweb2019'
+    });
+    
+    connection.connect();
+
+    var sql = `INSERT INTO usuarios VALUES (${email}, ${senha}, ${nome});`; 
+
+    connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      connection.end();
+      response.send('Não foi possível realizar o cadastro!');
+    });
+    
+    connection.end();
+    response.send('Email cadastradado: ' + email);
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
